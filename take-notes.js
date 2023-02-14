cc351749562448b6b68491baaa80848a51765122
ACB <a-bach@live.dk>
2023-02-14 12:24:02 +0100

    Enhance model

--- take-notes.js
+++ take-notes.js
@@ -1,28 +1,33 @@
 javascript: (async function () {
   const text = prompt("Insert your document text here");
   console.log("text", text);
-  const chunks = text.match(/.{1,1000}/gs);
+  const chunks = text.match(/.{1,10000}/gs);
   console.log(chunks);
   function isGenerating() {
     return document.querySelectorAll(".absolute.p-1")[0].innerHTML.indexOf('<div class="text-2xl"><span') > -1;
   }
-  function isSleeping() {
+  function sleep(milliseconds) {
+    if (typeof milliseconds === "undefined") {
+      milliseconds = 10000;
+    }
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve();
-      }, 5000);
+      }, milliseconds);
     });
   }
   for (let index = 0; index < chunks.length; index++) {
     let generating = isGenerating();
     while (generating) {
-      await isSleeping();
+      await sleep();
       generating = isGenerating();
     }
     const promptForChatGPT = "Create notes for the following text using a bulletlist. Text:" + chunks[index];
     const element = document.querySelectorAll(".resize-none")[0];
+    await sleep(1000);
     element.value = promptForChatGPT;
-    element.dispatchEvent(new Event("input", { bubbles: true }));
+    await sleep(1000);
     document.querySelectorAll(".absolute.p-1")[0].click();
+    await sleep(1000);
   }
 })();
