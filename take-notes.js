b4cd7f023ed603df5337425e7dc6bf58085888b9
ACB <a-bach@live.dk>
2023-02-14 09:46:44 +0100

    Add more logic to the model

--- take-notes.js
+++ take-notes.js
@@ -1,26 +1,28 @@
-javascript: (function () {
+javascript: (async function () {
   const text = prompt("Insert your document text here");
   console.log("text", text);
   const chunks = text.match(/.{1,1000}/gs);
   console.log(chunks);
-
   function isGenerating() {
     return document.querySelectorAll(".absolute.p-1")[0].innerHTML.indexOf('<div class="text-2xl"><span') > -1;
   }
-
-  for (let index = 0; index < array.length; index++) {
-    const promptForChatGPT = "Create notes for the following text using a bulletlist. Text:" + chunks[0];
+  function isSleeping() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 5000);
+    });
+  }
+  for (let index = 0; index < chunks.length; index++) {
+    let generating = isGenerating();
+    while (generating) {
+      await isSleeping();
+      generating = isGenerating();
+    }
+    const promptForChatGPT = "Create notes for the following text using a bulletlist. Text:" + chunks[index];
     const element = document.querySelectorAll(".resize-none")[0];
     element.value = promptForChatGPT;
     element.dispatchEvent(new Event("input", { bubbles: true }));
     document.querySelectorAll(".absolute.p-1")[0].click();
-
-    let running = running();
-
-    while (running) {
-      setTimeout(() => {
-        running = isGenerating();
-      }, 1000);
-    }
   }
 })();
