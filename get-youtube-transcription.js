2b1e62887f8cf6a08109f3daf424822960c95e94
Asbjørn Christian Bach <43601099+acbach@users.noreply.github.com>
2024-05-11 13:51:53 +0200

    Update get-youtube-transcription.js

--- get-youtube-transcription.js
+++ get-youtube-transcription.js
@@ -1,4 +1,14 @@
 javascript: (async function () {
-const text = document.getElementById("segments-container").textContent;
-  console.log(text);
+    // Select the div by ID
+    const div = document.getElementById("segments-container")
+
+    // Check if the div exists
+    if (div) {
+        // Retrieve the text content of the div, replace newlines with an empty string, and return the result
+        alert("See console for transcription");
+        console.log(div.textContent.replace(/\n/g, ''));
+    } else {
+        // Return a message if the div does not exist
+        alert('Transcription not found 😊');
+    }
 })();
