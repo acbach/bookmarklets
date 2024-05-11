9afb927ff00b27ac476977f0f54aa5eaa7e366a8
Asbjørn Christian Bach <43601099+acbach@users.noreply.github.com>
2024-05-11 12:34:33 +0200

    Update get-youtube-transcription.js

--- get-youtube-transcription.js
+++ get-youtube-transcription.js
@@ -1,33 +1,4 @@
 javascript: (async function () {
-  const text = prompt("Insert your document text here");
-  console.log("text", text);
-  const chunks = text.match(/.{1,15500}/gs);
-  console.log(chunks);
-  function isGenerating() {
-    return document.querySelectorAll(".absolute.p-1")[0].innerHTML.indexOf('<div class="text-2xl"><span') > -1;
-  }
-  function sleep(milliseconds) {
-    if (typeof milliseconds === "undefined") {
-      milliseconds = 10000;
-    }
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, milliseconds);
-    });
-  }
-  for (let index = 0; index < chunks.length; index++) {
-    let generating = isGenerating();
-    while (generating) {
-      await sleep();
-      generating = isGenerating();
-    }
-    const promptForChatGPT = "Create notes for the following text using a bulletlist.\nText: " + chunks[index].replace(/(\r\n|\n|\r)/gm, " ");
-    const element = document.querySelectorAll(".resize-none")[0];
-    await sleep(1000);
-    element.value = promptForChatGPT;
-    await sleep(1000);
-    document.querySelectorAll(".absolute.p-1")[0].click();
-    await sleep(1000);
-  }
+const text = document.getElementById("segments-container").textContent;
+  console.log(text);
 })();
