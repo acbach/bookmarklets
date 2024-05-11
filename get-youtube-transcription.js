1fd13a6cb4b3304cb8775bf639d3730f67cf7fb4
Asbjørn Christian Bach <43601099+acbach@users.noreply.github.com>
2024-05-11 15:50:43 +0200

    Update get-youtube-transcription.js

--- get-youtube-transcription.js
+++ get-youtube-transcription.js
@@ -1,8 +1,12 @@
 javascript: (async function () {
-    const div = document.getElementById("segments-container");
-    if (div) {
+    const elements = document.getElementsByClassName("segment-text");
+    if (elements.length>0) {
         alert("See console for transcription");
-        console.log(div.textContent.replace(/\n/g, ''));
+        let text = "";
+        for(var i = 0; i<elements.length-1; i++){
+            text = text + elements[i].textContent.replace(/\n/g, '') + " ";
+        }
+        console.log(text);
     } else {
         alert('Transcription not found 😊. Go to description -> click show transcription');
     }
