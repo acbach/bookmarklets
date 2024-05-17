5ab9c20ed35e270a0e4dffb49de1d46f46e14b54
Asbjørn Christian Bach <43601099+acbach@users.noreply.github.com>
2024-05-17 21:41:02 +0200

    Update get-youtube-transcription.js

--- get-youtube-transcription.js
+++ get-youtube-transcription.js
@@ -8,11 +8,8 @@ javascript: (async function () {
 
         const chunkSize = 25000*3;
         for (let start = 0; start < text.length; start += chunkSize) {
-            console.log(text.substring(start, start + chunkSize));
-
-            console.log("------------------------------------------------");
-            console.log("------------------------------------------------");
-            console.log("------------------------------------------------");
+            const entry = text.substring(start, start + chunkSize);
+            console.log(entry);
             console.log("------------------------------------------------");
             console.log("------------------------------------------------");
             console.log("------------------------------------------------");
